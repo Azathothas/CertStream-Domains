96ffb5b794314693efb4b09ce0b94f4b37193d05
Ajam <AjamX101@gmail.com>
2023-10-20 11:07:17 +0545

    Update scrape_certs_certstream_httpx_gowitness.sh

--- .github/scripts/scrape_certs_certstream_httpx_gowitness.sh
+++ .github/scripts/scrape_certs_certstream_httpx_gowitness.sh
@@ -9,7 +9,7 @@
  sort -u "/tmp/resolvers.txt" -o "/tmp/resolvers.txt"
 #---------------------# 
 #HTTPx (General)
-# httpx -l "$GITHUB_WORKSPACE/main/Data/np_ccTLDs/certstream_domains_np_all_24h.txt" -r "/tmp/resolvers.txt" -ip -status-code -title -location -follow-host-redirects -include-chain -server -tech-detect -cdn -content-type -content-length -websocket -random-agent -filter-regex "The plain HTTP request was sent to" -disable-update-check -silent -no-color | tee "$GITHUB_WORKSPACE/main/Data/np_ccTLDs/certstream_domains_np_all_24h_httpx.txt"
+ httpx -l "$GITHUB_WORKSPACE/main/Data/np_ccTLDs/certstream_domains_np_all_24h.txt" -r "/tmp/resolvers.txt" -ip -status-code -title -location -follow-host-redirects -include-chain -server -tech-detect -cdn -content-type -content-length -websocket -random-agent -filter-regex "The plain HTTP request was sent to" -disable-update-check -silent -no-color | tee "$GITHUB_WORKSPACE/main/Data/np_ccTLDs/certstream_domains_np_all_24h_httpx.txt"
 #Git Pull
  cd "$GITHUB_WORKSPACE/main" && git pull origin main  
 #---------------------# 
@@ -20,7 +20,7 @@ cat "$GITHUB_WORKSPACE/main/Data/np_ccTLDs/certstream_domains_np_all_24h_httpx.t
 #output dir is autocreated
 #Run gowitness
 # 1000 Hosts Screenshot ~ 30-45 mins
- echo ""
+ echo -e "\n[+] Total Hosts --> 📷 : $(wc -l < /tmp/subs_live.txt)\n"
  gowitness file -f "/tmp/subs_live.txt" --fullpage --disable-db --debug --screenshot-path "/tmp/goshots"
 #Remove Dupes (Prefer Https)
  fdupes "/tmp/goshots" --order=name | grep -v '/https-' | xargs -I {} find {} -type f -delete
