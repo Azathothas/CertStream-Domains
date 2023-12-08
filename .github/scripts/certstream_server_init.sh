7670715b795b7f94c2443db9450730788e895ecd
Ajam <AjamX101@gmail.com>
2023-12-08 14:25:49 +0545

    Create certstream_server_init.sh

---
+++ .github/scripts/certstream_server_init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Run (Install Less)
+# bash <(curl -qfsSL "https://@raw.githubusercontent.com/Azathothas/CertStream-Domains/main/.github/scripts/certstream_server_init.sh")
+
+##CertStream
+pip uninstall certstream --yes 2>/dev/null
+mkdir -p "$HOME/bin"
+#btop
+curl -qfsSL "https://raw.githubusercontent.com/Azathothas/Toolpacks/main/x86_64/btop" -o "$HOME/bin/btop" ; chmod +xwr "$HOME/bin/btop"
+#croc
+curl -qfsSL "https://raw.githubusercontent.com/Azathothas/Toolpacks/main/x86_64/croc" -o "$HOME/bin/croc" ; chmod +xwr "$HOME/bin/croc"
+#eget
+curl -qfsSL "https://raw.githubusercontent.com/Azathothas/Toolpacks/main/x86_64/eget" -o "$HOME/bin/eget" ; chmod +xwr "$HOME/bin/eget"
+#certstream
+curl -qfsSL "https://raw.githubusercontent.com/Azathothas/Toolpacks/main/x86_64/certstream" -o "$HOME/bin/certstream" ; chmod +xwr "$HOME/bin/certstream"
+#certstream-server-go
+curl -qfsSL "https://raw.githubusercontent.com/Azathothas/Toolpacks/main/x86_64/certstream-server-go" -o "$HOME/bin/certstream-server-go" ; chmod +xwr "$HOME/bin/certstream-server-go"
+#Configure certstream-server-go
+#kill zombie server
+pgrep -f "certstream-server-go" | xargs kill -9 2>/dev/null
+rm "/tmp/server_config.yaml" 2>/dev/null 
+#Get Latest Config
+wget --quiet "https://raw.githubusercontent.com/Azathothas/Arsenal/main/certstream/server_config.yaml" -O "/tmp/server_config.yaml"
+#Start Server  
+nohup "$HOME/bin/certstream-server-go" -config "/tmp/server_config.yaml" >/dev/null 2>&1 &
+netstat -tulpen
+#EOF
